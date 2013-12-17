852b193443e494c14ae9f6283c840ae4c393de18
Bryan Ngo <bngo92@gmail.com>
2013-12-16 17:07:08 -0800

    Consecutive test case

--- main/tests.py
+++ main/tests.py
@@ -6,11 +6,16 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from main.models import *
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class CardTest(TestCase):
+    def test_consecutive(self):
+        two = Card('', TWO)
+        three = Card('', THREE)
+        four = Card('', FOUR)
+
+        self.assertFalse(two.is_consecutive(two, THREE))
+        self.assertTrue(two.is_consecutive(three, FOUR))
+        self.assertFalse(two.is_consecutive(three, THREE))
+        self.assertTrue(two.is_consecutive(four, THREE))
